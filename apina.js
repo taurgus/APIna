2caeeb8d69ad2e7b703e9d69197235bc2e65a660
taurgus <100350635+taurgus@users.noreply.github.com>
2024-04-11 10:37:23 +0300

    apinoita indexissä

--- apina.js
+++ apina.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
+
 // ladataan .env muuttujat
 require('dotenv').config();
 
@@ -12,6 +13,7 @@ const MONGODB_URI = process.env.MONGODB_URI;
 
 // Middleware
 app.use(express.json());
+app.use(express.static('public'));
 
 /////////////////////////////// CONNECT TO MONGODB ///////////////////////////////
 mongoose.connect(MONGODB_URI, {
