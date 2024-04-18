94a46eb346d8729ec8b7df2bd6e675d4a07ccc76
taurgus <100350635+taurgus@users.noreply.github.com>
2024-04-18 11:39:07 +0300

    vittu

---
+++ routes/monkeys.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+const Monkey = require('../models/monkey');
+
+router.post('/', async (req, res) => {
+    console.log("Received body:", req.body); 
+
+    const { race} = req.body;
+    try {
+        const newMonkey = new Monkey({ race});
+        await newMonkey.save();
+        res.status(201).json(newMonkey);
+    } catch (error) {
+        console.error("Failed to save monkey:", error);
+        res.status(400).json({ message: error.message });
+    }
+});
+
+module.exports = router;
