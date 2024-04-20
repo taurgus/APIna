524073ca04d903f03db1b31668d0b3efd200bb7f
taurgus <100350635+taurgus@users.noreply.github.com>
2024-04-20 13:57:14 +0300

    parser

--- apina.js
+++ apina.js
@@ -10,10 +10,12 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 3007;
 const MONGODB_URI = process.env.MONGODB_URI;
+const bodyParser = require('body-parser');
+
 
 // Middleware
 app.use(express.json());
-
+app.use(bodyParser.json());
 app.use(express.static('public'));
 
 /////////////////////////////// CONNECT TO MONGODB ///////////////////////////////
--- models/monkey.js
+++ models/monkey.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
 const monkeySchema = new mongoose.Schema({
-    race: { type: String, required: true },
-    size: { type: String, required: true },
-    livingArea: { type: String, required: true }
+    race: { type: String},
+    size: { type: String},
+    livingArea: { type: String}
 });
 
 const Monkey = mongoose.model('Monkey', monkeySchema);
