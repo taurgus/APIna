efae04ac55643af5ef2376d09034eabfa1bf669c
taurgus <100350635+taurgus@users.noreply.github.com>
2024-04-20 15:39:01 +0300

    jee

--- apina.js
+++ apina.js
@@ -1,42 +1,41 @@
-//APIna by Arvi Klemetti, Riku Piipponen
+// APIna by Arvi Klemetti, Riku Piipponen
 
 const express = require('express');
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
-
-// .env 
-require('dotenv').config();
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3007;
 const MONGODB_URI = process.env.MONGODB_URI;
 
-
 // Middleware
-app.use(express.json());
+app.use(express.json()); // Add this line to parse the request body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-/////////////////////////////// CONNECT TO MONGODB ///////////////////////////////
-async function connectDB() {
+// Function to connect to MongoDB
+async function connectToMongoDB(uri) {
   try {
-      await mongoose.connect(MONGODB_URI, {
-          useNewUrlParser: true,
-          useUnifiedTopology: true
-      });
-      console.log('Connected to MongoDB');
+    await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    console.log('Successfully connected to MongoDB');
   } catch (error) {
-      console.error('Error connecting to MongoDB:', error);
+    console.error('Error connecting to MongoDB:', error.message);
   }
 }
 
-connectDB();
+// Connect to MongoDB
+connectToMongoDB(MONGODB_URI);
+
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
-//////////////////////////////////////////////////////////////////////////////////
 // Require Monkey routes
 const monkeyRouter = require('./routes/monkeys');
 
--- routes/monkeys.js
+++ routes/monkeys.js
@@ -1,19 +1,25 @@
 const express = require('express');
 const router = express.Router();
-const Monkey = require('../models/monkey');
+const Monkey = require('../models/monkey'); 
+
 
 router.post('/', async (req, res) => {
-    console.log("Received body:", req.body); 
+  const { race, size, livingArea } = req.body;
 
-    const { race} = req.body;
-    try {
-        const newMonkey = new Monkey({ race});
-        await newMonkey.save();
-        res.status(201).json(newMonkey);
-    } catch (error) {
-        console.error("Failed to save monkey:", error);
-        res.status(400).json({ message: error.message });
-    }
+  try {
+    const newMonkey = new Monkey({
+      race,
+      size,
+      livingArea
+    });
+
+    const savedMonkey = await newMonkey.save();
+
+    res.json(savedMonkey);
+  } catch (error) {
+    console.error("Apinan teko ei onnistunut:", error);
+    res.status(500).json({ message: error.message });
+  }
 });
 
 module.exports = router;
